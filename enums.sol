96a74086c535ea5b0c1908b8cc1ed88fc20b5a67
rgermanm <r.german.molina@gmail.com>
2023-12-02 20:18:35 -0300

    Prueba

---
+++ enums.sol
@@ -0,0 +1,19 @@
+pragma solidity >0.8.0;
+pragma experimental ABIEncoderV2;
+contract ejemplos_enums{
+
+ //Enumeracion de interuptor
+    enum estado {ON,OFF}
+
+//Variable del tipo enum estado
+    estado state;
+
+    function encender() public{
+        state = estado.ON;
+    }    
+
+    function apagar() public{
+        state = estado(1);
+    }
+
+}
